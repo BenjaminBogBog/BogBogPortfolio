3a34c474dfb6221517aa8e319174c406d63573bf
BenjaminBogBog <Benjaminbogbog@gmail.com>
2024-04-28 04:15:21 +0700

    new version

--- chicken_runner_backend/Backend/Game/ServiceWorker.js
+++ chicken_runner_backend/Backend/Game/ServiceWorker.js
@@ -1,6 +1,33 @@
+const cacheName = "BogBog-Chickapoo-0.0.15";
+const contentToCache = [
+    "Build/0.0.16.loader.js",
+    "Build/0.0.16.framework.js.gz",
+    "Build/0.0.16.data.gz",
+    "Build/0.0.16.wasm.gz",
+    "TemplateData/style.css"
+
+];
 
 self.addEventListener('install', function (e) {
     console.log('[Service Worker] Install');
     
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
 });
 
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
